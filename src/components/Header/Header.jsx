483ac2e10d42df2b5e19c49dab773d98829edbda
Joaopaulo2006 <jpo54545@gmail.com>
2025-02-11 22:25:42 -0300

    main

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LogoHeader from "../Logo/LogoHeader/LogoHeader.jsx";
+function Header() {
+  return (
+    <div>
+      <LogoHeader />
+      <p>Hello World!!</p>
+    </div>
+  );
+}
+export default Header;
---
+++ src/components/Logo/LogoHeader/LogoHeader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Logo from "../../../assets/logo-header.svg";
+
+const LogoHeader = () => {
+  return (
+    <img src={Logo} alt="Logo Header Drip Store" width={253} height={44} />
+  );
+};
+export default LogoHeader;
